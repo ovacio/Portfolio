530c4a8930cd6cb45c264df0ebeaceaed2401d8d
Daniil Kokolov <kokolovdaniil@gmail.com>
2024-12-21 19:40:47 +0300

    feature/forntend-14 🍎Update routes

--- src/shared/constants/routes.ts
+++ src/shared/constants/routes.ts
@@ -2,6 +2,6 @@ const isProd = process.env.NODE_ENV === 'production'
 
 export const ROUTES = {
   ROOT: isProd ? `${process.env.NEXT_PUBLIC_BASE_PATH_PRODUCTION}` : '/',
-  PROJECTS: isProd ? `${process.env.NEXT_PUBLIC_BASE_PATH_PRODUCTION}projects` : '/projects',
-  SOCIAL: isProd ? `${process.env.NEXT_PUBLIC_BASE_PATH_PRODUCTION}social` : '/social'
+  PROJECTS: isProd ? `${process.env.NEXT_PUBLIC_BASE_PATH_PRODUCTION}/projects` : '/projects',
+  SOCIAL: isProd ? `${process.env.NEXT_PUBLIC_BASE_PATH_PRODUCTION}/social` : '/social'
 }
