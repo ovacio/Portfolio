f8abecaa66f4cbb6b8a1e576d65ccfacb2f614f1
Daniil Kokolov <kokolovdaniil@gmail.com>
2024-12-21 18:46:48 +0300

    feature/frontend-06 🍎 Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/nextjs-portfolio',
+  basePath: '',
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true
