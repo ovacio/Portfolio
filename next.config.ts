d35f431df097027c6997953d627bab35807280c0
Daniil Kokolov <kokolovdaniil@gmail.com>
2024-12-21 19:03:50 +0300

    feature/forntend-09 🍎Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
+  basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
   output: 'export',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true
