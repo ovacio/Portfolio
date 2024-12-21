3f1035be7f05e7b7630d00d7142d4bada37b6766
Daniil Kokolov <kokolovdaniil@gmail.com>
2024-12-21 19:08:19 +0300

    feature/forntend-10 🍎Add env.production

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
-  basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
+  basePath: isProd ? process.env.BASE_PATH_PRODUCTION : '',
   output: 'export',
   reactStrictMode: true,
   poweredByHeader: false,
