c73f59d269dc2fec7f1165263e83be607da3a794
Daniil Kokolov <kokolovdaniil@gmail.com>
2024-12-21 19:25:19 +0300

    feature/forntend-11 🍎Update static logic

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
-  basePath: isProd ? process.env.BASE_PATH_PRODUCTION : '',
-  output: 'export',
+  basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH_PRODUCTION : '',
+  output: isProd ? 'export' : undefined,
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true
--- src/shared/constants/routes.ts
+++ src/shared/constants/routes.ts
@@ -1,5 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 export const ROUTES = {
-  ROOT: '/',
-  PROJECTS: `${process.env.NEXT_PUBLIC_BASE_PATH}/Projects`,
-  SOCIAL: `${process.env.NEXT_PUBLIC_BASE_PATH}/Social`
+  ROOT: isProd ? `${process.env.NEXT_PUBLIC_BASE_PATH_PRODUCTION}/` : '/',
+  PROJECTS: isProd ? `${process.env.NEXT_PUBLIC_BASE_PATH_PRODUCTION}/projects` : '/projects',
+  SOCIAL: isProd ? `${process.env.NEXT_PUBLIC_BASE_PATH_PRODUCTION}/social` : '/social'
 }
